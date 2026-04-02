f80ef92d7f917bf27f04305b8f5c4df6730bbe14
ifenetangerine <irenet@mit.edu>
2026-04-02 19:04:13 -0400

    all changes to particle_filter

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -9,50 +9,13 @@ import rclpy
 
 assert rclpy
 
-import math
+# added
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 from sensor_msgs.msg import PointCloud2, LaserScan
 from sensor_msgs_py import point_cloud2
 from std_msgs.msg import Header
 from geometry_msgs.msg import TransformStamped
-
-def euler_from_quaternion(quat_xyzw):
-    x, y, z, w = quat_xyzw
-
-    # roll (x-axis rotation)
-    sinr_cosp = 2.0 * (w * x + y * z)
-    cosr_cosp = 1.0 - 2.0 * (x * x + y * y)
-    roll = math.atan2(sinr_cosp, cosr_cosp)
-
-    # pitch (y-axis rotation)
-    sinp = 2.0 * (w * y - z * x)
-    if abs(sinp) >= 1.0:
-        pitch = math.copysign(math.pi / 2.0, sinp)
-    else:
-        pitch = math.asin(sinp)
-
-    # yaw (z-axis rotation)
-    siny_cosp = 2.0 * (w * z + x * y)
-    cosy_cosp = 1.0 - 2.0 * (y * y + z * z)
-    yaw = math.atan2(siny_cosp, cosy_cosp)
-
-    return roll, pitch, yaw
-
-
-def quaternion_from_euler(roll, pitch, yaw):
-    cr = math.cos(roll * 0.5)
-    sr = math.sin(roll * 0.5)
-    cp = math.cos(pitch * 0.5)
-    sp = math.sin(pitch * 0.5)
-    cy = math.cos(yaw * 0.5)
-    sy = math.sin(yaw * 0.5)
-
-    w = cr * cp * cy + sr * sp * sy
-    x = sr * cp * cy - cr * sp * sy
-    y = cr * sp * cy + sr * cp * sy
-    z = cr * cp * sy - sr * sp * cy
-
-    return x, y, z, w
 
 class ParticleFilter(Node):
 
@@ -115,11 +78,6 @@ class ParticleFilter(Node):
 
         self.num_particles = self.get_parameter("num_particles").value # number of particles we are using
 
-        # Initialize particles to a default pose so callbacks don't crash before /initialpose.
-        self.particles = np.zeros((self.num_particles, 3), dtype=float)
-
-        self.last_odom_info = None
-
         self.get_logger().info("=============+READY+=============")
 
         # Implement the MCL algorithm
@@ -132,6 +90,10 @@ class ParticleFilter(Node):
         # Publish a transformation frame between the map
         # and the particle_filter_frame.
 
+        self.last_odom_info  = np.array([0, 0, 0])
+
+        self.particles = None
+
 
 
 
@@ -143,6 +105,8 @@ class ParticleFilter(Node):
         Args:
             laser_msg (_type_): LaserScan
         """
+        if self.particles is None:
+            return
         observation = laser_msg.ranges
         weights = self.sensor_model.evaluate(self.particles, observation)
         self.particles = self.sensor_model.resample(self.particles,weights)
@@ -155,26 +119,28 @@ class ParticleFilter(Node):
         Args:
             odometry_msg (_type_): Odometry
         """
-
+        if self.particles is None:
+            return
         # Get the current xyz position of the robot
         current_odom_pose = odometry_msg.pose.pose
 
         # Get the rotation (yaw from the euler angles)
         current_odom_quat = current_odom_pose.orientation
         current_odom_quat_list = [current_odom_quat.x, current_odom_quat.y, current_odom_quat.z, current_odom_quat.w]
-        _, _, current_odom_yaw = euler_from_quaternion(current_odom_quat_list)
+
+        # get it in euler
+        r = R.from_quat(current_odom_quat_list)
+        current_odom_yaw = r.as_euler('xyz')[2]
+
 
         current_odom_info = np.array([current_odom_pose.position.x, current_odom_pose.position.y, current_odom_yaw])
 
-        if self.last_odom_info is None:
-            self.last_odom_info = current_odom_info
-            return
-
         # Subtract from last saved odometry to get the change in odometry deltax
         odom_change = current_odom_info - self.last_odom_info
-        self.last_odom_info = current_odom_info
 
         self.particles = self.motion_model.evaluate(self.particles, odom_change)
+
+        self.last_odom = current_odom
 
 
     def pose_callback(self, pose_msg):
@@ -193,7 +159,8 @@ class ParticleFilter(Node):
         # get the rotation
         rotation_quat = pose.orientation
         rotation_quat_list = [rotation_quat.x, rotation_quat.y, rotation_quat.z, rotation_quat.w]
-        _, _, theta = euler_from_quaternion(rotation_quat_list)
+
+        theta = R.from_quat(quat_list).as_euler('xyz')[2]
 
         x, y = pose.position.x, pose.position.y
         self.particles = np.broadcast_to((x,y,theta), (self.num_particles, 3))
@@ -237,7 +204,7 @@ class ParticleFilter(Node):
         t.transform.translation.y = y_pos
         t.transform.translation.z = 0
 
-        x, y, z, w = quaternion_from_euler(0.0, 0.0, theta)
+        x, y, z, w = R.from_euler('z', theta).as_quat()
 
         t.transform.rotation.x = x
         t.transform.rotation.y = y
